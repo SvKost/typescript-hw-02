b30bcfcd119ef7b54b6900217206b9f495d74013
SvKost <skost0634610557@gmail.com>
2024-04-26 13:39:49 +0300

    LoadMoreBtn.jsx was typed

--- src/components/LoadMoreBtn/LoadMoreBtn.tsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -1,6 +1,10 @@
 import css from "./LoadMoreBtn.module.css";
 
-const LoadMoreBtn = ({ onLoadMore }) => {
+type LoadMoreBtnProps = {
+  onLoadMore: () => void;
+};
+
+const LoadMoreBtn = ({ onLoadMore }: LoadMoreBtnProps) => {
   return (
     <button className={css.loadMoreBtn} type="button" onClick={onLoadMore}>
       Load more
